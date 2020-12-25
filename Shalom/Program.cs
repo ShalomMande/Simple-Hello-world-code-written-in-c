841d59ddd31357d1e4586563b805f270eef4a669
shalom mande <shalom.mande@gmail.com>
2020-12-24 21:11:07 -0500

    Add project files.

---
+++ Shalom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shalom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
